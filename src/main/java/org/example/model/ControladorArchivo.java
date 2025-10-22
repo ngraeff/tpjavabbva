86aaecf02a0b14485bd98fb3b83f6b8e1a376bfa
Nico Graeff <nicolasgraeff02@gmail.com>
2025-10-22 20:05:28 -0300

    mejoras finales

--- src/main/java/org/example/model/ControladorArchivo.java
+++ src/main/java/org/example/model/ControladorArchivo.java
@@ -8,26 +8,40 @@ import java.util.*;
 
 public class ControladorArchivo {
 
+    /**
+     * Graba el txt con las tareas cargadas en el programa.
+     *
+     * @param archivo direccion del archivo a grabar.
+     * @param tareas tareas cargadas en el programa.
+     */
     public void guardarEnTxt(String archivo,List<Tarea> tareas) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo)))  {
             for (Tarea t : tareas) {
                 writer.write(t.toString());
                 writer.newLine();
             }
-            System.out.println("Tareas guardadas en " + archivo);
         } catch (IOException e) {
             System.out.println("Error al guardar: " + e.getMessage());
         }
     }
 
-    public List<Tarea> cargarDesdeTxt(String archivo,Integer siguienteId) {
-        List<Tarea> tareas = new ArrayList<>();
+    /**
+     * Carga al programa las tareas ya cargadas que están en un txt.
+     *
+     * @param archivo direccion del archivo a cargar.
+     * @param siguienteId id de la siguiente tarea para cargar.
+     * @param tareas tareas ya cargadas en el programa.
+     * @return lista de la Tareas cargadas.
+     */
+    public List<Tarea> cargarDesdeTxt(String archivo,Integer siguienteId, List<Tarea> tareas) {
+
         try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
             String linea;
             while ((linea = reader.readLine()) != null) {
 
                 String[] partes = linea.split(",");
-                int id = Integer.parseInt(partes[0].split(" ")[1]);
+                //int id = Integer.parseInt(partes[0].split(" ")[1]);
+                int id = siguienteId;
                 String descripcion = partes[1].split(" ")[1];
                 boolean completada = partes[2].split(" ")[1].equalsIgnoreCase("COMPLETADA");
 
@@ -42,9 +56,8 @@ public class ControladorArchivo {
                     tarea.marcarCompletada();
                     tareas.add(tarea);
                 }
-                siguienteId = Math.max(siguienteId, id + 1);
+                siguienteId++;
             }
-            System.out.println("Tareas cargadas desde " + archivo);
         } catch (IOException e) {
             System.out.println("No se pudo cargar: " + e.getMessage());
         }
--- src/main/java/org/example/model/Menu.java
+++ src/main/java/org/example/model/Menu.java
@@ -17,27 +17,25 @@ public class Menu {
         this.escaner = new Scanner(System.in);
     }
 
+    /**
+     * Loop principal del programa.
+     * Ejecuta la funcion para ver las opciones y
+     * evaluar el ingreso del usuario.
+     *
+     */
     public void start() {
         int opcion;
         do {
-            opcion = -1;
             mostrarOpciones();
-            boolean entradaValida = false;
-            while (!entradaValida) {
-                System.out.print("Ingrese una opción numérica: ");
-                if (escaner.hasNextInt()) {
-                    opcion = escaner.nextInt();
-                    escaner.nextLine();
-                    entradaValida = true;
-                } else {
-                    System.out.println("Error: solo se permiten números.");
-                    escaner.nextLine();
-                }
-            }
+            opcion = solicitarNumero("Ingrese una opcion del menu: ");
             evaluarOpcion(opcion);
         } while (opcion != 0);
     }
 
+    /**
+     * Imprime por pantalla todas las funciones del menu.
+     *
+     */
     public void mostrarOpciones() {
         System.out.println("MENÚ:");
         System.out.println("1️⃣ Agregar tarea");
@@ -48,12 +46,16 @@ public class Menu {
         System.out.println("6️⃣ Marcar tarea como completada");
         System.out.println("7️⃣ Ordenar tareas por fecha de vencimiento de menor a mayor");
         System.out.println("8️⃣ Ordenar tareas por fecha de vencimiento de mayor a menor");
-        System.out.println("9\uFE0F⃣Cargar tareas de archivo");
-        System.out.println("🔟Grabar tareas de archivo");
+        System.out.println("9\uFE0F⃣ Cargar tareas de archivo");
+        System.out.println("🔟 Grabar tareas de archivo");
         System.out.println("0️⃣ Salir");
-        System.out.print("Seleccione una opción: ");
     }
 
+    /**
+     * Evalua el ingreso del usuario y llama a la
+     * funcion correspondiente.
+     * @param opcion opcion ingresada por el usuario
+     */
     public void evaluarOpcion(int opcion) {
         switch (opcion) {
             case 1 -> agregarTarea();
@@ -71,6 +73,10 @@ public class Menu {
         }
     }
 
+    /**
+     * Solicita al usuario la descripcion de la tarea y la
+     * agrega a la lista.
+     */
     public void agregarTarea() {
         String descripcion;
         do {
@@ -86,10 +92,26 @@ public class Menu {
             int numero = tareas.get(tareas.size()-1).getId() + 1;
             tareas.add(new Tarea(numero, descripcion));
         }
+
+
+        //limpiarConsolaFor();
+        limpiarConsola();
+
+        System.out.println("Agregando tarea...");
+        tiempoEspera(1000);
         System.out.println("Tarea agregada.");
+        tiempoEspera(1000);
+        //limpiarConsolaFor();
+        limpiarConsola();
+
     }
 
+    /**
+     * Lista todas las tareas cargadas.
+     */
     public void listarTodasLasTareas() {
+        //limpiarConsolaFor();
+        limpiarConsola();
         if (this.tareas.isEmpty()) {
             System.out.println("Listado de tareas vacio.");
         }else {
@@ -97,11 +119,16 @@ public class Menu {
                 System.out.println(tarea.toString());
             }
         }
+        tiempoEspera(2000);
     }
 
+    /**
+     * Lista las tareas que ya fueron completadas.
+     */
     public void listarTareasCompletas() {
         long cantidadDeTareasCompletas = this.tareas.stream().filter(tarea -> tarea.isCompletada()).count();
-
+        //limpiarConsolaFor();
+        limpiarConsola();
         if (cantidadDeTareasCompletas == 0) {
             System.out.println("No hay tareas completas");
         }else{
@@ -111,12 +138,18 @@ public class Menu {
                 }
             }
         }
+        tiempoEspera(2000);
 
     }
 
+    /**
+     * Lista las tareas que todavia no fueron completadas.
+     */
     public void listarTareasPendientes() {
         long cantidadDeTareasPendientes = this.tareas.stream().filter(tarea -> !tarea.isCompletada()).count();
 
+        //limpiarConsolaFor();
+        limpiarConsola();
         if (cantidadDeTareasPendientes == 0){
             System.out.println("No hay tareas pendientes");
         } else{
@@ -126,11 +159,14 @@ public class Menu {
                 }
             }
         }
+        tiempoEspera(2000);
     }
 
+    /**
+     * Solicita al usuario ID y elimina la tarea con el ID ingresado.
+     */
     public void eliminarTareaPorId() {
-        System.out.print("Ingrese el id de la tarea: ");
-        int id = escaner.nextInt();
+        int id = solicitarNumero("Ingrese el id de la tarea: ");
         try {
             sacarTarea(id);
             System.out.println("Tarea eliminada.");
@@ -140,9 +176,12 @@ public class Menu {
         }
     }
 
+    /**
+     * Solicita al usuario ID y marca como completada la tarea con el ID ingresado.
+     */
     public void marcarTareaCompletada() {
-        System.out.print("Ingrese el ID de la tarea a completar: ");
-        int id = escaner.nextInt();
+
+        int id = solicitarNumero("Ingrese el ID de la tarea a completar: ");
         if (tareas.isEmpty()) {
             System.out.println("No hay tareas listadas.");
         }else  {
@@ -164,8 +203,12 @@ public class Menu {
         }
     }
 
+    /**
+     * Lista las tareas ordenadas por fecha de vencimiento de menor a mayor
+     */
     public void ordenarPorFechaDeVencimientoDeMenorAMayor(){
-
+        //limpiarConsolaFor();
+        limpiarConsola();
         if (tareas.isEmpty()){
             System.out.println("No hay tareas listadas.");
         }else{
@@ -173,10 +216,15 @@ public class Menu {
             System.out.println("Tareas ordenadas por fecha de vencimiento:");
             tareas.forEach(System.out::println);
         }
+        tiempoEspera(2000);
     }
 
+    /**
+     * Lista las tareas ordenadas por fecha de vencimiento de mayor a menor
+     */
     public void ordenarPorFechaDeVencimientoDeMayorAMenor(){
-
+        //limpiarConsolaFor();
+        limpiarConsola();
         if (tareas.isEmpty()) {
             System.out.println("No hay tareas listadas.");
         }
@@ -185,29 +233,146 @@ public class Menu {
             System.out.println("Tareas ordenadas por fecha de vencimiento:");
             tareas.forEach(System.out::println);
         }
+        tiempoEspera(2000);
     }
 
+    /**
+     * Carga las tareas que se encuentran en el archivo
+     */
     public void cargarTareasDeArchivo() {
-
+        int cantidadDeTareasInicial = this.tareas.size();
         String archivo = "src/main/resources/tareas.txt";
+        //C:\Users\Nicolas Daniel G\IdeaProjects\tpjavabbva\src\main\resources\tareas.txt
         ControladorArchivo controlador = new ControladorArchivo();
         if (tareas.isEmpty()){
-            this.tareas = controlador.cargarDesdeTxt(archivo, 1);
+            this.tareas = controlador.cargarDesdeTxt(archivo, 1,tareas);
 
         }else {
-            this.tareas = controlador.cargarDesdeTxt(archivo,tareas.get(tareas.size()-1).getId() + 1);
+            this.tareas = controlador.cargarDesdeTxt(archivo,tareas.get(tareas.size()-1).getId() + 1,tareas);
         }
+
+        //limpiarConsolaFor();
+        limpiarConsola();
+        System.out.println("Cargando tareas...");
+        tiempoEspera(1000);
+        if (this.tareas.isEmpty()){
+            System.out.println("No hay tareas para cargar.");
+        }else{
+            int cantidadDeTareasFinal = this.tareas.size();
+            System.out.println("Se cargaron " + (cantidadDeTareasFinal-cantidadDeTareasInicial) + " tareas");
+        }
+        tiempoEspera(1000);
+        //limpiarConsolaFor();
+        limpiarConsola();
     }
 
+    /**
+     * Graba las tareas cargadas a un archivo txt
+     */
     public void grabarTareasAArchivo() {
         String archivo = "src/main/resources/tareas.txt";
         ControladorArchivo controlador = new ControladorArchivo();
         controlador.guardarEnTxt(archivo,tareas);
+
+        //limpiarConsolaFor();
+        limpiarConsola();
+        System.out.println("grabando tareas...");
+        tiempoEspera(1000);
+        if (this.tareas.isEmpty()){
+            System.out.println("No hay tareas para grabar.");
+        }else{
+            int cantidadDeTareas = this.tareas.size();
+            System.out.println("Se grabaron " + cantidadDeTareas + " tareas");
+        }
+        tiempoEspera(1000);
+        //limpiarConsolaFor();
+        limpiarConsola();
     }
 
+    /**
+     * Elimina una tarea de la lista según su ID.
+     *
+     * @param idTarea ID de la tarea que se desea eliminar
+     * @throws TareaInexistenteException si no se encuentra ninguna tarea con el ID ingresado
+     */
     private void sacarTarea(int idTarea) throws TareaInexistenteException {
+        //limpiarConsolaFor();
+        limpiarConsola();
+        System.out.println("Sacando tarea " + idTarea + "...");
+        tiempoEspera(1000);
         if (!tareas.removeIf(t -> t.getId() == idTarea)){
             throw new TareaInexistenteException("La tarea es inexistente");
         }
+        System.out.println("La tarea " + idTarea + " fue eliminada con exito");
+        tiempoEspera(1000);
+        //limpiarConsolaFor();
+        limpiarConsola();
+
+
+    }
+
+    /**
+     * Limpia la consola imprimiendo 50 espacios
+     */
+    public void limpiarConsolaFor(){
+        for (int i = 0; i<50;i++){
+            System.out.println(" ");
+        }
+    }
+    /**
+     * Limpia la consola usando ProcessBuilder (solo por terminal)
+     */
+    // solo funciona en la consola (no en Inteliji)
+    private void limpiarConsola() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("No se pudo limpiar la consola.");
+        }
+    }
+
+    /**
+     * Genera un delay en la ejecucion
+     * @param milisegundos cantidad de milisegundos para la espera
+     */
+    private void tiempoEspera(int milisegundos){
+        try {
+            Thread.sleep(milisegundos); //
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Solicita un ingreso al usuario y valida que este mismo cumpla condiciones para que
+     * sea correcto.
+     *
+     * @param textoAPedir texto que se muestra al usuario a la hora de hacer el ingreso.
+     * @return numero ingresado
+     */
+    private int solicitarNumero(String textoAPedir) {
+        int opcion = -1;
+        boolean entradaValida = false;
+
+        while (!entradaValida) {
+            System.out.print(textoAPedir);
+            String linea = escaner.nextLine().trim();
+
+            if (linea.isEmpty()) {
+                System.out.println("Error: no se ingresó ningún valor.");
+                continue;
+            }
+            try {
+                opcion = Integer.parseInt(linea);
+                entradaValida = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Error: solo se permiten números.");
+            }
+        }
+        return opcion;
     }
 }
